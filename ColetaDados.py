a83e92137ea38cfc8c6226571ef8fbe50a178728
Leandro de Almeida <leandroleekrap@gmail.com>
2020-06-06 18:45:23 -0300

    realizando buscas 'arrays' e salvando resultados em JSON

--- ColetaDados.py
+++ ColetaDados.py
@@ -2,25 +2,29 @@ import json
 
 from selenium.webdriver import Firefox
 from time import sleep
+from pprint import pprint
 
-url = "https://crane-technology.herokuapp.com/conteudos"
+browser = Firefox()
+browser.get("https://crane-technology.herokuapp.com/conteudos")
+resultado = {}
 
-navegado = Firefox()
-navegado.get(url)
+def get_link(browser, elemento):
+    element = browser.find_element_by_tag_name(elemento)
+    ancoras = element.find_elements_by_tag_name('h1')
+    for acora in ancoras:
+        resultado[acora.text] = acora.get_attribute('href')
+    return resultado
 
-#esperar 3 segundos antes de procurar outro elemento
-sleep(3)
+sleep(2)
 
-a = navegado.find_element_by_id('h3-texto')
 
-print(f' valor da acao: {a.text}')
-gravando = a.text
+pprint(get_link(browser, 'main'))
 
 # gravar arquivo
-gravando_json = json.dumps(gravando, indent=1)
+gravando_json = json.dumps(resultado, indent=1)
 with open("crane.json", "w") as outfile:
     outfile.write(gravando_json)
 
 #finalizar apos busca
-navegado.quit()
+browser.quit()
 
--- scraping-aula-04.py
+++ scraping-aula-04.py
@@ -1,17 +1,23 @@
 from selenium.webdriver import Firefox
 from urllib.parse import  urlparse
 from time import sleep
+from pprint import pprint
 
 browser = Firefox()
 browser.get("https://selenium.dunossauro.live/aula_04.html")
 
-#url_parceada = urlparse(browser.current_url)
+def get_link(browser, elemento):
+    resultado = {}
+    element = browser.find_element_by_tag_name(elemento)
+    ancoras = element.find_elements_by_tag_name('a')
+    for acora in ancoras:
+        resultado[acora.text] = acora.get_attribute('href')
+    return resultado
 
 sleep(2)
-aside = browser.find_element_by_tag_name('aside')
-aside_acoras = aside.find_elements_by_tag_name('a')
 
-for acora in aside_acoras:
-    print(acora.text)
-    print(acora.text, acora.get_attribute('href'))
-    print('--------------------------------------------\n')
+
+pprint(get_link(browser, 'aside'))
+
+
+#main = browser.find_element_by_tag_name('main')
